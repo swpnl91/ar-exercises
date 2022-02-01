f59e291c1042c6b3e68102a2fa581413c4d5fb9e
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-02-01 00:32:00 +0000

    exercise 6 complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Tom", last_name: "Holland", hourly_rate: 100)
+@store1.employees.create(first_name: "Timmy", last_name: "Chalamet", hourly_rate: 200)
+
+@store2.employees.create(first_name: "Andrew", last_name: "Garfield", hourly_rate: 300)
+
+employee_four = Employee.create(store_id: 2, first_name: "Swapnil", last_name: "Parab", hourly_rate:5000)
