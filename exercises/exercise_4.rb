d62befda0128b3e385d1b24b187eb934650ce69d
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-02-01 00:13:29 +0000

    exercise 4 complete

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,19 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+store_four = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store_five = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store_six = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+ puts store.name, store.annual_revenue
+end
+
+@womens_stores = Store.where(Store.arel_table[:womens_apparel].eq(true).and(Store.arel_table[:annual_revenue].lt(1000000)))
+
+@womens_stores.each do |store|
+  puts store.name, store.annual_revenue
+end
